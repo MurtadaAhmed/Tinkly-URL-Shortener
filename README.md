5e6cf32fa0e8fbcb1a4220b0e792b0cd21831c70
Murtada Ahmed <murtadhabg@gmail.com>
2025-01-31 19:18:32 +0200

    add database.py

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # URL-Shortener
+
+
+https://chatgpt.com/c/679baae0-3090-800e-abad-094b25dc1471
---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./shortener.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class URL(Base):
+    __tablename__ = "urls"
+
+    shortcode = Column(String, primary_key=True, index=True)
+    long_url = Column(String, nullable=False)
+    visit_count= Column(Integer, default=0)
+
+Base.metadata.create_all(bind=engine)
