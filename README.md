956abb4fc3b0ae883ec5de381f6ae484927d6796
Murtada Ahmed <murtadhabg@gmail.com>
2025-02-01 20:03:26 +0200

    updated style

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 **Tinkly** is a simple and fast URL shortening service built using **FastAPI** and **SQLAlchemy** for database interaction. This project allows users to shorten URLs, redirect them, and track the number of visits for each short URL.
 
+![img.png](img.png)
 ---
 
 ## Features
--- static/script.js
+++ static/script.js
@@ -10,8 +10,10 @@ document.getElementById("shorten-btn").addEventListener("click", async function(
     let data = await response.json();
     let shortUrlContainer =  document.getElementById("short_url");
     if (data.short_url === undefined) {
+        shortUrlContainer.className = "short-url-container"
         shortUrlContainer.innerHTML = `<p class="error-message">Enter full URL (with http:// or https://)</p>`;
     } else {
+        shortUrlContainer.className = "short-url-container"
         shortUrlContainer.innerHTML = `<p>Short URL:</p> 
                                         <a href="${data.short_url}" target="_blank">${data.short_url}</a>`;
     }
