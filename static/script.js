d5893c278591291a1543415b592dc5fe1c99ee7b
Murtada Ahmed <murtadhabg@gmail.com>
2025-02-02 14:39:13 +0200

    update js to accept button enter for input

--- static/script.js
+++ static/script.js
@@ -1,20 +1,28 @@
-document.getElementById("shorten-btn").addEventListener("click", async function() {
+document.getElementById("shorten-btn").addEventListener("click", shortenUrl);
+document.getElementById("long_url").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        shortenUrl();
+    }
+});
+
+async function shortenUrl() {
     let longURL = document.getElementById("long_url").value;
 
     let response = await fetch("/shorten/", {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({long_url: longURL})
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ long_url: longURL })
     });
 
     let data = await response.json();
-    let shortUrlContainer =  document.getElementById("short_url");
+    let shortUrlContainer = document.getElementById("short_url");
+
+    shortUrlContainer.className = "short-url-container";
+
     if (data.short_url === undefined) {
-        shortUrlContainer.className = "short-url-container"
         shortUrlContainer.innerHTML = `<p class="error-message">Enter full URL (with http:// or https://)</p>`;
     } else {
-        shortUrlContainer.className = "short-url-container"
         shortUrlContainer.innerHTML = `<p>Short URL:</p> 
                                         <a href="${data.short_url}" target="_blank">${data.short_url}</a>`;
     }
-})
+}
